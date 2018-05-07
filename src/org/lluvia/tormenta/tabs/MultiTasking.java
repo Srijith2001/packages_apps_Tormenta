CandyRoms/packages_apps_CandyShop

Multitasking: Fix Screenshot editor app preference FC
Change-Id: Ie9e6500d3763d182e9fa35b0a7c928cef9bd8dda

src/org/candy/candyshop/tabs/MultiTasking.java
@@ -100,8 +100,13 @@ public class MultiTasking extends SettingsPreferenceFragment implements
     }
 
     @Override
-    public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.CANDYSHOP;
+    public int getDialogMetricsCategory(int dialogId) {
+        switch (dialogId) {
+            case DIALOG_SCREENSHOT_EDIT_APP:
+                return MetricsEvent.CANDYSHOP;
+            default:
+                return 0;
+        }
     }
 
     @Override
@@ -123,6 +128,11 @@ public class MultiTasking extends SettingsPreferenceFragment implements
                     Toast.LENGTH_LONG).show();
         }
         return true;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.CANDYSHOP;
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {

Srijith2001/packages_apps_Tormenta

src/org/lluvia/tormenta/tabs/MultiTasking.java
@@ -100,8 +100,13 @@ public class MultiTasking extends SettingsPreferenceFragment implements
     }
 
     @Override
-    public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.TORMENTA;
+    public int getDialogMetricsCategory(int dialogId) {
+        switch (dialogId) {
+            case DIALOG_SCREENSHOT_EDIT_APP:
+                return MetricsEvent.TORMENTA;
+            default:
+                return 0;
+        }
     }
 
     @Override
@@ -123,6 +128,11 @@ public class MultiTasking extends SettingsPreferenceFragment implements
                     Toast.LENGTH_LONG).show();
         }
         return true;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.TORMENTA;
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {